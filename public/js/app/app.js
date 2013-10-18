f15bb7fd58785858e23af2f4e82ac453e281127f
Ashley Wright <AshleyMarieEvans@gmail.com>
2013-10-18 10:52:41 -0500

    Template

---
+++ public/js/app/app.js
@@ -0,0 +1,55 @@
+'use strict';
+
+$(document).ready(initialize);
+
+function initialize(fn, flag){
+  if(!canRun(flag)) {return;}
+
+  $(document).foundation();
+}
+
+// -------------------------------------------------------------------- //
+
+
+
+
+// -------------------------------------------------------------------- //
+
+function getValue(selector, fn){
+  var value = $(selector).val();
+  value = value.trim();
+  $(selector).val('');
+
+  if(fn){
+    value = fn(value);
+  }
+
+  return value;
+}
+
+function parseUpperCase(string){
+  return string.toUpperCase();
+}
+
+function parseLowerCase(string){
+  return string.toLowerCase();
+}
+
+function formatCurrency(number){
+  return '$' + number.toFixed(2);
+}
+
+// -------------------------------------------------------------------- //
+// -------------------------------------------------------------------- //
+// -------------------------------------------------------------------- //
+
+function canRun(flag){
+  var isQunit = $('#qunit').length > 0;
+  var isFlag = flag !== undefined;
+  var value = isQunit && isFlag || !isQunit;
+  return value;
+}
+
+// -------------------------------------------------------------------- //
+// -------------------------------------------------------------------- //
+// -------------------------------------------------------------------- //
